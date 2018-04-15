12437f40e78308cca761df608f752fc67c94fa69
SuperCourierYangyufan <34188376+SuperCourierYangyufan@users.noreply.github.com>
2018-04-15 18:54:39 +0800

    Add files via upload

---
+++ e3-common/src/main/java/cn/e3/common/pojo/SearchResult.java
@@ -0,0 +1,37 @@
+package cn.e3.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by YangYuFan on 2018/4/15.
+ */
+public class SearchResult implements Serializable{
+    private long recordCount;
+    private int totalPages;
+    private List<SearchItem> itemList;
+
+    public long getRecordCount() {
+        return recordCount;
+    }
+
+    public void setRecordCount(long recordCount) {
+        this.recordCount = recordCount;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<SearchItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<SearchItem> itemList) {
+        this.itemList = itemList;
+    }
+}
